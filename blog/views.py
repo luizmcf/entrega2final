32a06daec394420c6e7d383a8317553b54260386
Luiz Eduardo Fernandes <luiz.mcf97@usp.br>
2023-11-22 19:40:34 -0300

    Segundo comit com categorias

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import redirect, get_object_or_404, render
 
-from.models import Post
+from .forms import CommentForm
+from .models import Post, Category
 
-def detail(request, slug):
+def detail(request, category_slug, slug):
     post = get_object_or_404(Post, slug=slug)
     
-    return render(request, 'blog/detail.html', {'post': post})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+
+        return render(request,'blog/detail.html', {'post': post, 'form': form})
+
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    
+    return render(request,'blog/category.html', {'category': category})
+
+    
